866723202b3cca9bf1eccdfff5564d8f20b66ab5
kellyzhaokl <xinyue.zhao.2023@mitb.smu.edu.sg>
2024-03-27 23:48:28 +0800

    update

--- shiny_test/app.R
+++ shiny_test/app.R
@@ -1,6 +1,24 @@
-pacman::p_load(igraph, tidygraph, ggraph, 
-               visNetwork, lubridate, clock,forecast,
-               tidyverse,graphlayouts, tmap,bslib, shinyalert,terra,gstat,sp,sf,leaflet)
+#library(gstat)
+#pacman::p_load(igraph, tidygraph, ggraph, 
+#               visNetwork, lubridate, clock,forecast,
+#              tidyverse,graphlayouts, tmap,bslib, shinyalert,terra,sp,sf,leaflet,gstat)
+library(gstat)
+library(igraph)
+library(tidygraph)
+library(ggraph)
+library(visNetwork)
+library(lubridate)
+library(clock)
+library(forecast)
+library(tidyverse)
+library(graphlayouts)
+library(tmap)
+library(bslib)
+library(shinyalert)
+library(terra)
+library(sp)
+library(sf)
+library(leaflet)
 
 
 # Read the data
@@ -13,37 +31,9 @@ links1 <- read_csv("data/mc3_links_new1.csv")
 merged_data <- read_csv("data/merged_data.csv")
 monthly_data_filtered <- read_csv("data/monthly_data_filtered.csv")
 #zxy
-data <- read_csv("data/weather.csv", na = c("?", "�"))
-
-data <- data %>%
-  dplyr::filter(Year >= 2014, Year <= 2023)
-
-colnames(data) <- c(
-  'Station', 'Year', 'Month', 'Day', 'DailyRainfall',
-  'Highest30minRainfall', 'Highest60minRainfall', 'Highest120minRainfall',
-  'MeanTemperature', 'MaxTemperature', 'MinTemperature',
-  'MeanWindSpeed', 'MaxWindSpeed'
-)
-
-data <- data %>%
-  mutate(
-    DailyRainfall = as.numeric(DailyRainfall),
-    Highest30minRainfall = as.numeric(Highest30minRainfall),
-    Highest60minRainfall = as.numeric(Highest60minRainfall),
-    Highest120minRainfall = as.numeric(Highest120minRainfall),
-    MeanTemperature = as.numeric(MeanTemperature),
-    MaxTemperature = as.numeric(MaxTemperature),
-    MinTemperature = as.numeric(MinTemperature)
-  ) %>%
-  suppressWarnings()
-
-
-# Get the rainfall station dataset
-rfstations1 <- read.csv("data/aspatial/RainfallStation.csv")
-
+data <- read_csv("data/cleaned_data.csv")
 mpsz2019 <-st_read(dsn = "data/geospatial",layer ="MPSZ-2019") %>%
   st_transform(CRS =3414)
-
 
 ui <- fluidPage(
   useShinyalert(force = TRUE),
@@ -163,11 +153,12 @@ ui <- fluidPage(
                )
              )
     ),
-    # 添加 Rainfall Analysis 选项卡
+    #Rainfall Analysis
     tabPanel("Rainfall in Singapore",
              flowLayout(
                numericInput("year_rain", "Year", value = 2023, min = 2014, max = 2023, step = 1),
                numericInput("month_rain", "Month", value = 1, min = 1, max = 12, step = 1),
+               numericInput("nmax", "nmax(IDW)",value = 10, min = 1, max = 15, step = 1)
              ),
              fluidRow(
                column(6,
@@ -178,15 +169,17 @@ ui <- fluidPage(
                ),
              )
     ),
-    # 添加 Prediction 选项卡
+    # Prediction
     tabPanel("Rainfall Prediction",
              flowLayout(
-               sliderInput("predict_year", "Predict Year", value = 2024, min = 2024, max = 2026, step = 1, sep = ""),
+               sliderInput("predict_year", "Predict Year", value = 2024, min = 2024, max = 2030, step = 1, sep = ""),
+               selectInput("show_forecast", "Show Forecast", choices = c("Yes", "No"), selected = "Yes"),
+               selectInput("show_ci", "Show 95% CI", choices = c("Yes", "No"), selected = "Yes")
              ),
              fluidRow(
                column(12,
-                      plotOutput("predict_plot") %>% shinycssloaders::withSpinner(type = 5)
-               ),
+                      plotOutput("predict_plot")
+               )
              )
     )
   ),
@@ -196,10 +189,10 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-
+  
   shinyalert(
-    title = "First Time Here?",
-    text = "Hi! Welcome to N.E.M.O. Kindly read the user guide before proceeding.",
+    title = "Hi,First Time Here?",
+    text = "Welcome to explore the weather. Kindly read the user guide before proceeding.",
     size = "l",
     closeOnEsc = TRUE,
     closeOnClickOutside = FALSE,
@@ -207,7 +200,7 @@ server <- function(input, output) {
     type = "info",
     showConfirmButton = TRUE,
     showCancelButton = FALSE,
-    confirmButtonText = "I have read the User Guide",
+    confirmButtonText = "YES. I have read the User Guide",
     confirmButtonCol = "#AEDEF4",
     timer = 0,
     imageUrl = "",
@@ -222,29 +215,29 @@ server <- function(input, output) {
   df_year = reactive({
     data %>% dplyr::filter(Year == input$year_rain)
   })
-
+  
   df_year_month = reactive({
     df_year() %>% dplyr::filter(Month == input$month_rain)
   })
-
+  
   ### Page 1 output
-
+  
   output$anomPlot <- renderVisNetwork({
-
+    
     afilter_nodes <- nodes %>%
       filter(group == input$entity & revenue_group == input$revenue & transboundary == input$transboundary)
-
+    
     afilter_links <- links %>%
       filter(source %in% afilter_nodes$id | target %in% afilter_nodes$id)
-
+    
     adistinct_source <- afilter_links %>%
       distinct(source) %>%
       rename("id" = "source")
-
+    
     adistinct_target <- afilter_links %>%
       distinct(target) %>%
       rename("id" = "target")
-
+    
     atotal_nodes <- bind_rows(adistinct_source, adistinct_target)
     atotal_links <- afilter_links %>%
       rename("from" = "source",
@@ -273,38 +266,38 @@ server <- function(input, output) {
       ) %>%
       visInteraction(navigationButtons = FALSE)
   })
-
+  
   ### Page 2 Output
-
+  
   output$similarityPlot <- renderPlot({
-
+    
     # Filter nodes data from input$industry
     filtered_nodes <- nodes1 %>%
       filter(if (input$industry == "All") TRUE else group == input$industry)
-
+    
     # Filter links based filtered_nodes to get connections
     filtered_links <- links1 %>%
       filter(source %in% filtered_nodes$id)
-
+    
     # Get unique source and target
     links_source <- filtered_links %>%
       distinct(source) %>%
       rename("id" = "source")
-
+    
     links_target <- filtered_links %>%
       distinct(target) %>%
       rename("id" = "target")
-
+    
     # bind links to get overall nodes dataframe
     filtered_nodes_new <- bind_rows(links_source, links_target) %>%
       left_join(nodes, by = "id") %>%
       select(id, group)
-
+    
     # Create graph object
     filtered_graph <- tbl_graph(nodes = filtered_nodes_new,
                                 edges = filtered_links,
                                 directed = FALSE)
-
+    
     # Calculate all Similarity Measures first
     filtered_graph <- filtered_graph %>%
       activate(nodes) %>%
@@ -316,7 +309,7 @@ server <- function(input, output) {
         closeness = closeness(filtered_graph),
         page_rank = page_rank(filtered_graph)$vector
       )
-
+    
     set.seed(1234)
     ggraph(filtered_graph,
            layout = "nicely"
@@ -350,42 +343,42 @@ server <- function(input, output) {
         legend.title = element_text()
       )
   })
-
+  
   ### Page 3 output
   output$tempMap <- renderTmap({
     # 过滤数据
     selected_data <- merged_data[merged_data$Year == input$year & merged_data$Month == input$month, ]
-
+    
     if(nrow(selected_data) == 0) {
       return(NULL) # 如果没有数据，不渲染地图
     }
-
+    
     selected_sf <- st_as_sf(selected_data, coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant") %>%
       st_transform(crs = 3414)
-
+    
     # 加载地理空间数据
     mpsz2019 <- st_read(dsn = "data/geospatial", layer = "MPSZ-2019") %>%
       st_transform(crs = 3414)
-
+    
     # 检查并修复无效的几何形状
     if(any(!st_is_valid(mpsz2019))) {
       mpsz2019 <- st_make_valid(mpsz2019)
     }
-
+    
     # 创建地图
     pal <- colorNumeric(palette = "YlOrRd", domain = selected_sf$MonthlyAvgTemp)
     tmap_mode("view")
-
+    
     tm <- tm_shape(mpsz2019) +
       tm_borders() +
       tm_shape(selected_sf) +
       tm_dots(col = "MonthlyAvgTemp", palette = "YlOrRd", size = 0.2,
               popup.vars = c("Station" = "Station", "Year" = "Year", "Month" = "Month", "Avg Temp(°C)" = "MonthlyAvgTemp")) +
       tm_layout(title = paste(input$year,"-",input$month, "Average Temperature in Singapore"))
-
+    
     tm
   })
-    #page:cluster
+  #page:cluster
   output$clusterPlot <- renderPlot({
     # 根据用户选择的年份过滤数据
     filtered_data <- subset(monthly_data_filtered, Year == input$selectedYear)
@@ -412,36 +405,27 @@ server <- function(input, output) {
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
   })
   
-
+  
   ### Page 4 output
-
+  
   rfdata_sf1_obj = reactive({
     df = df_year_month()
-
+    
     rfdata1 <-  df %>%
       group_by(Station, Year, Month) %>%
       dplyr::summarise(MONTHSUM = sum(DailyRainfall, na.rm = TRUE), .groups = "drop") %>%
       ungroup()
-
-
-    # rfdata1 <- data %>%
-    #   dplyr::filter(Year == 2016, Month == 1) %>%
-    #   group_by(Station, Year, Month) %>%
-    #   dplyr::summarise(MONTHSUM = sum(DailyRainfall, na.rm = TRUE), .groups = "drop") %>%
-    #   ungroup()
-
+    
     rfdata1 <- rfdata1 %>%
       left_join(rfstations1) %>% drop_na()
-
+    
     rfdata_sf1 <- st_as_sf(rfdata1, coords = c("Longitude", "Latitude"), crs = 4326) %>%
       st_transform(crs = 3414)
-
-
   })
-
-
+  
+  
   output$map1_plot = renderTmap({
-
+    
     rfdata_sf1 = rfdata_sf1_obj()
     grid <- terra::rast(mpsz2019, nrows = 690, ncols = 1075)
     xy <- terra::xyFromCell(grid, 1:ncell(grid))
@@ -452,23 +436,20 @@ server <- function(input, output) {
     coop <- st_filter(coop,mpsz2019)
     res <- gstat(formula = MONTHSUM ~ 1,
                  locations = rfdata_sf1,
-                 nmax = 15,
+                 nmax = input$nmax, # 使用输入的 nmax 值
                  set = list(idp = 0))
-
+    
     rfdata_sf_crs1 <- st_crs(rfdata_sf1)
-
-    # print(rfdata_sf_crs1)
-
+    
     coop <- st_transform(coop, crs = rfdata_sf_crs1)
     resp <- predict(res,coop)
     resp <- st_transform(resp, crs = terra::crs(grid))
     resp$x <- st_coordinates(resp)[,1]
     resp$y <- st_coordinates(resp)[,2]
     resp$pred <- resp$var1.pred
-
+    
     pred <- terra::rasterize(resp, grid, field = "pred", fun = 'mean')
-    #print(terra::values(pred))
-
+    
     tmap_options(check.and.fix = TRUE)
     tmap_mode("view")
     tm_shape(pred) +
@@ -484,15 +465,15 @@ server <- function(input, output) {
       tm_compass(type="8star", size = 2) +
       tm_scale_bar() +
       tm_grid(alpha =0.2)
-
+    
   })
-
-
+  
+  
   output$map2_plot = renderTmap({
-
-
+    
+    
     rfdata_sf1 = rfdata_sf1_obj()
-
+    
     tmap_options(check.and.fix = TRUE)
     tmap_mode("view")
     tm_shape(mpsz2019)+
@@ -500,49 +481,80 @@ server <- function(input, output) {
       tm_shape(rfdata_sf1)+
       tm_dots(col="MONTHSUM",size = 0.12,
               popup.vars = c("Station" = "Station", "Year" = "Year", "Month" = "Month", "Month Sum" = "MONTHSUM"))
-
-
   })
-
+  
   output$predict_plot = renderPlot({
     data_filtered <- data %>%
       filter(!is.na(DailyRainfall))
-
+    
     rainfall_sum <- data_filtered %>%
       group_by(Year, Month, Station) %>%
       summarise(Rainfall = sum(DailyRainfall), .groups = "drop")
-
+    
     rainfall_avg <- rainfall_sum %>%
       group_by(Year, Month) %>%
       summarise(AvgRainfall = mean(Rainfall), .groups = "drop")
-
+    
     rain_ts <- rainfall_avg %>%
       ungroup() %>%
       transmute(AvgRainfall) %>%
       ts(start = c(2014, 1), freq = 12)
-
+    
     rain_ts1 <- window(rain_ts, start = c(2014, 1), end = c(2023, 12))
-
-    #Plot Time Series Data
-    start_year <- start(rain_ts1)[1]
-    start_month <- start(rain_ts1)[2]
-
-    dates <- seq(as.Date(paste(start_year, start_month, "01", sep = "-")), by = "month", length.out = length(rain_ts1))
-
-    rain_ts1_df <- data.frame(Date = dates, AvgRainfall = as.vector(rain_ts1))
-
-
-    hujan_train <- window(rain_ts1, end = c(2023,12))
-    # hujan_test <- window(rain_ts1, start = c(2024,1))
-
-    fit1 <- Arima(hujan_train, order=c(1,0,1), seasonal = c(1,0,1))
-
+    
+    hujan_train <- window(rain_ts1, end = c(2023, 12))
+    
+    fit1 <- Arima(hujan_train, order = c(1, 0, 1), seasonal = c(1, 0, 1))
+    
     forecast_result <- forecast(fit1, h = (input$predict_year - 2023) * 12)
-
-    plot(forecast_result)
-
+    
+    plot(rain_ts1,
+         main = "Rainfall Forecast",
+         ylab = "Average Rainfall",
+         xlab = "Year",
+         xlim = c(2014, input$predict_year),
+         ylim = c(0, max(rain_ts1, forecast_result$upper[, 2])),
+         type = "l",
+         col = "black",
+         lwd = 2
+    )
+    
+    if (input$show_forecast == "Yes") {
+      lines(forecast_result$mean, col = "green", lwd = 2)
+    }
+    
+    if (input$show_ci == "Yes") {
+      polygon(c(time(forecast_result$lower), rev(time(forecast_result$upper))),
+              c(forecast_result$lower[, 2], rev(forecast_result$upper[, 2])),
+              col = "lightskyblue",
+              border = NA
+      )
+    }
+    
+    legend_items <- c("Observed")
+    legend_colors <- c("black")
+    legend_lty <- c(1)
+    
+    if (input$show_forecast == "Yes") {
+      legend_items <- c(legend_items, "Forecast")
+      legend_colors <- c(legend_colors, "green")
+      legend_lty <- c(legend_lty, 1)
+    }
+    
+    if (input$show_ci == "Yes") {
+      legend_items <- c(legend_items, "95% CI")
+      legend_colors <- c(legend_colors, "lightskyblue")
+      legend_lty <- c(legend_lty, 2)
+    }
+    
+    legend("topleft",
+           legend = legend_items,
+           col = legend_colors,
+           lty = legend_lty,
+           lwd = 2,
+           cex = 0.8
+    )
   })
-
 }
 
 # Run the application
